4e91567a0d5db4c6914d2ec5e15ed1f776b84a16
Vincent Ollivier <v@vinc.cc>
2017-01-30 21:08:36 +0000

    Auth user before actions on pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,6 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index]
 
   # GET /pictures
   # GET /pictures.json
