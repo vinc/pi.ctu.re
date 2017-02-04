ede8d613c9dc7463b2158b4e6b15155325845744
Vincent Ollivier <v@vinc.cc>
2017-02-04 19:17:15 +0100

    Rebuild gallery on history back

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -1,12 +1,10 @@
 $(document).on('turbolinks:load', function() {
-  $('#gallery.loading')
-    .removeClass('loading')
-    .justifiedGallery({
-      captions: false,
-      rowHeight: 200,
-      maxRowHeight: 250,
-      lastRow: 'nojustify',
-      margins: 10,
-      border: 0
-    });
+  $('#gallery').justifiedGallery({
+    captions: false,
+    rowHeight: 200,
+    maxRowHeight: 250,
+    lastRow: 'nojustify',
+    margins: 10,
+    border: 0
+  });
 });
