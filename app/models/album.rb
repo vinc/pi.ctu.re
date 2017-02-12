e1e626a2e4a569b5bb5bc3f48540d1a737175c1f
Vincent Ollivier <v@vinc.cc>
2017-02-12 20:50:11 +0100

    Fix albums order

--- app/models/album.rb
+++ app/models/album.rb
@@ -5,5 +5,5 @@ class Album < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :pictures
 
-  order_query :order_by_time, [:created_at, :desc]
+  order_query :order_by_time, [:updated_at, :desc]
 end
