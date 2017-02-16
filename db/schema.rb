2aad06df7fca77a3a229130ec4e728d6a3f740b8
Vincent Ollivier <v@vinc.cc>
2017-02-16 13:37:54 +0100

    Add is_admin to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216105152) do
+ActiveRecord::Schema.define(version: 20170216105751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20170216105152) do
     t.string   "fullname"
     t.string   "default_license",        default: "CC BY-NC-ND", null: false
     t.string   "avatar"
+    t.boolean  "is_admin",               default: false,         null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
