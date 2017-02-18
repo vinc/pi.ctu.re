94a6bb4c7946f1357e58c049e24fde2204d35454
Vincent Ollivier <v@vinc.cc>
2017-02-18 19:54:35 +0100

    Remove unused config for tests

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,22 +5,3 @@ CarrierWave.configure do |config|
     config.asset_host = 'http://localhost:4000';
   end
 end
-
-if Rails.env.test?
-  Dir["#{Rails.root}/app/uploaders/*.rb"].each { |file| require file }
-  if defined?(CarrierWave)
-    CarrierWave::Uploader::Base.descendants.each do |klass|
-      next if klass.anonymous?
-
-      klass.class_eval do
-        def cache_dir
-          "#{Rails.root}/spec/support/uploads/tmp"
-        end
-
-        def store_dir
-          "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-        end
-      end
-    end
-  end
-end
