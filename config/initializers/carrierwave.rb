2abe3ed31881e1067491477763789298df4058ca
Vincent Ollivier <v@vinc.cc>
2017-02-19 14:29:02 +0100

    Switch infra.stru.ctu.re to HTTPS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.asset_host = 'http://infra.stru.ctu.re';
+    config.asset_host = 'https://infra.stru.ctu.re';
   else
     config.asset_host = 'http://localhost:4000';
   end
