1b50f33db2b3aef2f0adc8b1df927d633a17bff9
Vincent Ollivier <v@vinc.cc>
2018-03-17 22:31:19 +0100

    Refactor invitation spec

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -13,26 +13,26 @@
 require "rails_helper"
 
 RSpec.describe Invitation, type: :model do
+  subject { FactoryBot.build(:invitation) }
+
   it "has a valid factory" do
-    invitation = FactoryBot.build(:invitation)
-    expect(invitation).to be_valid
+    expect(subject).to be_valid
   end
 
-  describe ".approve" do
+  describe "#approve" do
     it "approves invitation" do
-      invitation = FactoryBot.create(:invitation)
-      expect(invitation.approved_at).to be_nil
-      invitation.approve
-      expect(invitation.approved_at).not_to be_nil
+      expect(subject.approved_at).to be_nil
+      subject.approve
+      expect(subject.approved_at).not_to be_nil
     end
   end
 
-  describe "approved" do
-    it "filters approved invitations" do
-      5.times { FactoryBot.create(:invitation) }
-      expect(Invitation.approved.count).to eq(0)
+  describe ".approved" do
+    let!(:invitations) { FactoryBot.create_list(:invitation, 5) }
 
-      Invitation.first.approve
+    it "filters approved invitations" do
+      expect(Invitation.approved.count).to eq(0)
+      invitations.first.approve
       expect(Invitation.approved.count).to eq(1)
     end
   end
