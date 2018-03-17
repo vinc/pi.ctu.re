b325b8cdb0c7da88955bd10bc9fa0dac251372de
Vincent Ollivier <v@vinc.cc>
2018-03-17 20:35:18 +0100

    Increate coverage of UsersController

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,10 +3,11 @@ require "rails_helper"
 RSpec.describe UsersController, type: :controller do
   describe "GET #show" do
     context "when user exists" do
+      let(:user) { FactoryBot.create(:user) }
+
       it "returns http success" do
-        user = FactoryBot.create(:user)
         get :show, params: { username: user.username }
-        expect(response).to have_http_status(:success)
+        expect(response).to be_successful
       end
     end
 
@@ -15,6 +16,25 @@ RSpec.describe UsersController, type: :controller do
         expect do
           get :show, params: { username: "invalid" }
         end.to raise_exception(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+
+  context "with signed in user" do
+    let(:user) { FactoryBot.create(:user) }
+
+    before(:each) do
+      sign_in user
+    end
+
+    describe "PATCH #update" do
+      context "with valid user attributes" do
+        let(:user_attributes) { FactoryBot.attributes_for(:user) }
+
+        it "returns http redirect to #show" do
+          patch :update, params: { username: user.username, user: user_attributes }
+          expect(response).to redirect_to(user)
+        end
       end
     end
   end
