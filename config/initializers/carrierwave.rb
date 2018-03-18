cafaa0c4c29ed5397ad64c9a5b438c9d05f6c1f1
Vincent Ollivier <v@vinc.cc>
2018-03-18 07:57:54 +0100

    Add PICTURE_DELIVERY_URL env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,3 @@
 CarrierWave.configure do |config|
-  config.asset_host = if Rails.env.production?
-                        "https://infra.stru.ctu.re"
-                      else
-                        "http://localhost:4000"
-                      end
+  config.asset_host = ENV["PICTURE_DELIVERY_URL"]
 end
