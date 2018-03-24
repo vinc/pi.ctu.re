7f03c2042f1db67e3981bf4247ec8a628a437a67
Vincent Ollivier <v@vinc.cc>
2018-03-24 10:14:43 +0100

    Refactor welcome page

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -4,6 +4,8 @@ RSpec.feature "Home", type: :feature do
   scenario "User visits the homepage" do
     visit "/"
 
-    expect(page).to have_text("Welcome to Picture")
+    body_html = I18n.t("welcome.body_html")
+    body_text = ActionView::Base.full_sanitizer.sanitize(body_html)
+    expect(page).to have_text(body_text)
   end
 end
