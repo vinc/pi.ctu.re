d093b0ed842286ded429a41fedd66f8d94cf4315
Vincent Ollivier <v@vinc.cc>
2018-04-08 08:59:27 +0200

    Test admin generator with fake stdin

--- spec/lib/generators/admin_generator_spec.rb
+++ spec/lib/generators/admin_generator_spec.rb
@@ -37,4 +37,31 @@ describe AdminGenerator, type: :generator do
       expect(User.count).to eq(0)
     end
   end
+
+  context "with attributes from stdin" do
+    before(:each) do
+      admin = FactoryBot.build(:user)
+
+      fake_stdin(admin.slice(:email, :username, :password).values) do
+        run_generator ["admin"]
+      end
+    end
+
+    it "creates a user admin" do
+      created_admin = User.last
+      expect(created_admin).not_to be_nil
+      expect(created_admin.is_admin).to be true
+    end
+  end
+
+  private
+
+  def fake_stdin(*args)
+    $stdin = StringIO.new
+    $stdin.puts(args.shift) until args.empty?
+    $stdin.rewind
+    yield
+  ensure
+    $stdin = STDIN
+  end
 end
