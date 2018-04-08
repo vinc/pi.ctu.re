6ac7012ddb0a6a9411d53bfbe328c865400cc294
Vincent Ollivier <v@vinc.cc>
2018-04-08 19:42:18 +0200

    Add meta tags helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,19 @@
 module ApplicationHelper
+  def page_meta_tags
+    display_meta_tags(
+      site: "Picture",
+      separator: "-",
+      reverse: true,
+      icon: "/favicon.ico",
+      canonical: url_for(only_path: false),
+      og: {
+        title: :title,
+        site_name: :site,
+        url: :canonical
+      }
+    )
+  end
+
   def body_class
     "#{controller_path}/#{action_name}".tr("/", "-")
   end
