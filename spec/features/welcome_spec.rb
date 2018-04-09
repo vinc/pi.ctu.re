1ec1f72c87636f5d7c1246ebab106ff0ce7e14a4
Vincent Ollivier <v@vinc.cc>
2018-04-09 22:05:27 +0200

    Fix translation in tests

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Welcome", type: :feature do
   scenario "Guest visits the homepage" do
     visit "/"
 
-    body_html = I18n.t("welcome.body_html")
+    body_html = I18n.t("welcome.index.sections.what.title")
     body_text = ActionView::Base.full_sanitizer.sanitize(body_html)
     expect(page).to have_text(body_text)
   end
