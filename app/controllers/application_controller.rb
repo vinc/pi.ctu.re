dd8fe1b642a00c96f045ad53eb039eeae9443a1c
Vincent Ollivier <v@vinc.cc>
2018-04-09 22:00:05 +0200

    Devise needs default_url_options to be a class method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
-  def default_url_options
+  def self.default_url_options
     { locale: I18n.locale }
   end
 
