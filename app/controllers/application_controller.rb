82aab70cf8f5015542c6d8d2a9fd85e6a83ba2f5
Vincent Ollivier <v@vinc.cc>
2018-04-09 20:30:34 +0200

    Add locale param

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 
   def pundit_user
     current_user || User.new # guest user
