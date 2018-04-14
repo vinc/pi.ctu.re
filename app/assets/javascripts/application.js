1ac91cc30a51d0ba8ab0aaa2951048bb75bb163c
Vincent Ollivier <v@vinc.cc>
2018-04-14 09:18:26 +0200

    Add french timeago

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require popper.js
 //= require bootstrap-sprockets
 //= require rails-timeago
+//= require locales/jquery.timeago.fr.js
 //= require justifiedGallery/dist/js/jquery.justifiedGallery
 //= require dropzone
 //= require_tree .
