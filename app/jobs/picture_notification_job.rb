a7d3fe340a7e239b3ff673cf986afc0c66230796
Vincent Ollivier <v@vinc.cc>
2018-04-15 15:42:04 +0200

    Broadcast only public picture notifications

--- app/jobs/picture_notification_job.rb
+++ app/jobs/picture_notification_job.rb
@@ -4,6 +4,7 @@ class PictureNotificationJob < ApplicationJob
   def perform(picture)
     TimelineChannel.broadcast_to(picture.user, picture: picture.token)
 
+    return unless picture.public_setting?
     picture.user.followers.each do |follower|
       TimelineChannel.broadcast_to(follower, picture: picture.token)
     end
