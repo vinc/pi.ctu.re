1a7ffcf5f59bbd3d9fde0b644e17aa8723473216
Vincent Ollivier <v@vinc.cc>
2023-02-06 23:44:43 +0100

    Use rails 7.0 defaults

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module PictureApplication
   class Application < Rails::Application
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     config.active_support.cache_format_version = 7.0
 
